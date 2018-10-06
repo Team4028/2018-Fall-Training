14244e4a5fb959bcc2c962a6a32119d2a9e7ae7b
Tom Bruns <xtobr39@hotmail.com>
2018-10-06 08:03:57 -0400

    initial check-in

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,133 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.ExampleSubsystem;
+
+public class Robot extends TimedRobot 
+{
+  // global working variables
+  public static ExampleSubsystem _exampleSubsystem = new ExampleSubsystem();
+  public static OI _oi;
+
+  Command _autonCommand;
+  SendableChooser<Command> _autonChooser = new SendableChooser<>();
+
+  /**
+   * This function is run when the robot is first started up and should be used for any initialization code.
+   */
+  @Override
+  public void robotInit() 
+  {
+    _oi = new OI();
+    _autonChooser.addDefault("Default Auto", new ExampleCommand());
+    // _autonChooser.addObject("My Auto", new MyAutoCommand());
+    SmartDashboard.putData("Auto mode", _autonChooser);
+  }
+
+  // ==============================================================================================
+  // Autonomous Mode
+  // ==============================================================================================
+
+  /**
+   * This method run 1x when the robot is enabled in auton mode
+   */
+  @Override
+  public void autonomousInit() 
+  {
+    _autonCommand = _autonChooser.getSelected();
+
+    // schedule the autonomous command (example)
+    if (_autonCommand != null) {
+      _autonCommand.start();
+    }
+  }
+
+  /**
+   * This function is called periodically during autonomous.
+   */
+  @Override
+  public void autonomousPeriodic() 
+  {
+    Scheduler.getInstance().run();
+  }
+
+  // ==============================================================================================
+  // Telop Mode
+  // ==============================================================================================
+  @Override
+  public void teleopInit() {
+    // This makes sure that the autonomous stops running when teleop starts running. If you want the autonomous to
+    if (_autonCommand != null) 
+    {
+      _autonCommand.cancel();
+    }
+  }
+
+  /**
+   * This function is called periodically during operator control.
+   */
+  @Override
+  public void teleopPeriodic() 
+  {
+    Scheduler.getInstance().run();
+  }
+
+  // ==============================================================================================
+  // Test Mode
+  // ==============================================================================================
+
+  /**
+   * This function is called periodically during test mode.
+   */
+  @Override
+  public void testPeriodic() 
+  {
+  }
+
+  // ==============================================================================================
+  // Other Special Modes
+  // ==============================================================================================
+  /**
+   * This function is called every robot packet, no matter the mode. Use
+   * this for items like diagnostics that you want ran during disabled,
+   * autonomous, teleoperated and test.
+   *
+   * <p>This runs after the mode specific periodic functions, but before
+   * LiveWindow and SmartDashboard integrated updating.
+   */
+  @Override
+  public void robotPeriodic() 
+  {
+  }
+
+  /**
+   * This function is called once each time the robot enters Disabled mode.
+   * You can use it to reset any subsystem information you want to clear when
+   * the robot is disabled.
+   */
+  @Override
+  public void disabledInit() 
+  {
+  }
+
+  /**
+   * This function is called periodically when the robot is disabled
+   */
+  @Override
+  public void disabledPeriodic() 
+  {
+    Scheduler.getInstance().run();
+  }
+}
